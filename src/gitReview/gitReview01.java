bfbde902b6000909062360414227e91433cc7d0c
keroles295 <kerolesbotros295@gmail.com>
2022-06-01 14:21:47 -0400

    1st commit

---
+++ src/gitReview/gitReview01.java
@@ -0,0 +1,12 @@
+package gitReview;
+
+public class gitReview01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New start ");
+		System.out.println("New start because my inter ");
+
+	}
+
+}
